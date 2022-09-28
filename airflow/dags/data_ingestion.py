43b056fa527201863d9423fe7edded70ad7b56f9
Artem <leeartem95@gmail.com>
2022-09-28 09:40:37 +0000

    added upload to gsc task

--- airflow/dags/data_ingestion.py
+++ airflow/dags/data_ingestion.py
@@ -23,17 +23,34 @@ import pyarrow.csv as pv
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 url = f"https://fsa-catalogue2.s3.eu-west-2.amazonaws.com/Biotoxin+Results+2021+160322.csv"
 file_name_raw = f"Biotoxin+Results+2021+160322.csv"
+parquet_file = f"Biotoxin+Results+2021+160322.parquet"
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "retries": 1,
+    "retries": 0,
 }
 
 def convert_to_parquet(file_name):
     po = pv.ParseOptions(delimiter=',')
     table = pv.read_csv(file_name, parse_options=po)
     pq.write_table(table, file_name.replace('.csv', '.parquet'))
+
+
+def upload_to_gcs(bucket, object_name, local_file):
+
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # the maximum size is 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob1 = bucket.blob(object_name)
+    blob1.upload_from_filename(local_file)
+
 
 
 with DAG(
@@ -58,11 +75,17 @@ with DAG(
             "file_name": f"{path_to_local_home}/{file_name_raw}",
         },
     )
-    print_python_version_task = BashOperator(
-        task_id="print_python_version",
-        bash_command=f'python.__version__()'
+
+    upload_to_gcs_task = PythonOperator(
+        task_id="upload_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/{parquet_file}",
+            "local_file": f"{path_to_local_home}/{parquet_file}",
+        },
     )
 
 if __name__ == '__main__':
 
-    download_dataset_task >> convert_to_parquet_task >> print_python_version_task
+    download_dataset_task >> convert_to_parquet_task >> upload_to_gcs_task
