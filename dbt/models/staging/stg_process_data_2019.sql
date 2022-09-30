622809c3b9b3c91fefa0975c45bc55a7f0beefa7
Artem Li <leeartem95@gmail.com>
2022-09-30 13:40:32 +0000

    fixed the reported value in 2019

--- dbt/models/staging/stg_process_data_2019.sql
+++ dbt/models/staging/stg_process_data_2019.sql
@@ -7,7 +7,7 @@ SubmissionYear,
 Category_name as Category, 
 Scenario, 
 Gas,
-`Reported_Value` as Reported_Value
+cast(`Reported_Value` as numeric) as Reported_Value
 
 from {{ source('staging', 'GHG_projections_2019') }}
-limit 1000
+limit 100
